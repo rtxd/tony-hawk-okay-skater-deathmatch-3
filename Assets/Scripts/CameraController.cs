9528a448b590336accd2b9166957d42ff34a3b25
RTXD <alej.munoz@gmail.com>
2021-08-11 18:22:29 +1000

    Fixed camer follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject skater;
+    public Vector3 distanceFromSkater;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(skater.transform.position.x - distanceFromSkater.x, skater.transform.position.y - distanceFromSkater.y, skater.transform.position.z - distanceFromSkater.z);
+    }
+}
